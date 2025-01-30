cd40ae3d3b716f7c59376897fdf831ebec70b413
LeonardoTozzi <leonardotozzis@gmail.com>
2025-01-30 16:47:13 -0300

    Pratica05

---
+++ Pratica02/Pratica05.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+
+namespace Pratica05
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            double z;
+            x = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            z = x * y / 12.0;
+            Console.WriteLine(z.ToString("F3", CultureInfo.InvariantCulture));
+
+
+
+
+        }
+    }
+}
