706647b2aec4b50760a008d56821bbb7731bb363
LeonardoTozzi <leonardotozzis@gmail.com>
2025-01-30 15:33:52 -0300

    nova pratica

---
+++ Pratica02/Pratica03.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+
+namespace Pratica03
+{
+    class Pratica03
+    {
+        static void Main(string[] args)
+        {
+            int x; /*distância*/
+            double y, z; /*combustível*/
+
+            
+            x = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            
+
+            z = x / y;
+
+
+            Console.WriteLine(z.ToString("F3", CultureInfo.InvariantCulture) + " km/1");
+
+
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
