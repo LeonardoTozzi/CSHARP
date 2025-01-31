5a82221ceddb75a97f6b8919c026a75e2507b02d
LeonardoTozzi <leonardotozzis@gmail.com>
2025-01-31 16:35:26 -0300

    Novas praticas

---
+++ Pratica02/Pratica06.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+
+namespace Pratica06
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double fixo, totalvendas, salario;
+
+            nome = Console.ReadLine();
+            fixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            totalvendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salario = fixo + totalvendas * 15 / 100;
+
+            Console.WriteLine("TOTAL = R$ " + salario.ToString("F2", CultureInfo.InvariantCulture) );
+
+
+
+        }
+    }
+}
---
+++ Pratica02/Pratica07.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+
+namespace Pratica07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, pi, volume;
+            pi = 3.14159;
+
+            r = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            volume = (4 / 3.0) * pi * Math.Pow(r, 3);
+
+
+            Console.WriteLine("VOLUME = "+volume.ToString("F3", CultureInfo.InvariantCulture));
+            
+
+        }
+    }
+}
