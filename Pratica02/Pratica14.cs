62ef1120c83ee42877cea5e4beb58a1a28b88ae6
LeonardoTozzi <leonardotozzis@gmail.com>
2025-02-04 12:03:39 -0300

    Valor de conta

---
+++ Pratica02/Pratica14.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Pratica14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutos;
+            double valor;
+
+
+
+            minutos = int.Parse(Console.ReadLine());
+            valor = 50.00;
+            if (minutos > 100)
+            {
+                valor = valor + (minutos - 100) * 2.0;
+                Console.WriteLine("Valor a pagar: R$ " + valor.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                Console.WriteLine("Valor a pagar: R$ " + valor.ToString("F2", CultureInfo.InvariantCulture));
+
+            }
+
+        }
+    }
+}
