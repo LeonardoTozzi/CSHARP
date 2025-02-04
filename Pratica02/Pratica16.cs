b0a14d43b1c844321fba27e7ac20698ea24b5e35
LeonardoTozzi <leonardotozzis@gmail.com>
2025-02-04 16:15:22 -0300

    while

---
+++ Pratica02/Pratica16.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Pratica16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            String[] vet = Console.ReadLine().Split(' ');
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+
+            while (x != y)
+            {
+                if (x > y)
+                {
+                    Console.WriteLine("Decrescente");
+
+                }
+                else
+                {
+                    Console.WriteLine("Crscente");
+
+                }
+                vet = Console.ReadLine().Split(' ');
+                x = int.Parse(vet[0]);
+                y = int.Parse(vet[1]);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
