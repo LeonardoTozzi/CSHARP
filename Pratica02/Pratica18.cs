d1bba8ad16c925cbbac5315be25500356f3c29f3
LeonardoTozzi <leonardotozzis@gmail.com>
2025-02-05 13:05:16 -0300

    Utilizando FOR

---
+++ Pratica02/Pratica18.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Pratica18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, x, soma;
+
+            N = int.Parse(Console.ReadLine());
+            soma = 0;
+            for (int i = 1; i <= N; i = i++)
+            {
+                x = int.Parse(Console.ReadLine());
+                soma = soma + x;
+            }
+
+            Console.WriteLine(soma);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
