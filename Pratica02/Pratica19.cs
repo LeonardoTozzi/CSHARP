76839de4ad0d74299f06878368e284f4c8ec790b
LeonardoTozzi <leonardotozzis@gmail.com>
2025-02-07 15:11:16 -0300

    Usando FOR

---
+++ Pratica02/Pratica19.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Pratica19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+
+            valor = int.Parse(Console.ReadLine());
+            for (int i = 1 ; i<=10; i++)
+            {
+                int resultado = i * valor;
+                Console.WriteLine(i + " x " + valor + " = " + resultado);
+            }
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
