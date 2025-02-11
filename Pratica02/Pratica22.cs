00046f3845ee030504860d8efba82794921c3309
LeonardoTozzi <leonardotozzis@gmail.com>
2025-02-11 16:21:34 -0300

    Usando condição DO WHILE

---
+++ Pratica02/Pratica22.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Pratica22
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double C, F;
+            char repetir;
+            do
+            {
+                Console.Write("Digita a temperatura em Celsius: ");
+                C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                F = 9.0 * C / 5.0 + 32.0;
+                Console.WriteLine("Equivalente em Fahrenheit: " + F.ToString("F1", CultureInfo.InvariantCulture));
+                Console.Write("Deseja repetir (s/n)? ");
+                repetir = char.Parse(Console.ReadLine());
+
+
+            } while (repetir == 's');
+        }
+    }
+}
