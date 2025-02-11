1674b837693f03fa9939c68b001f00693786be3a
LeonardoTozzi <leonardotozzis@gmail.com>
2025-02-11 13:49:01 -0300

    Usando condição FOR

---
+++ Pratica02/Pratica21.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Pratica21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, y, x;
+
+            for (i = 1; i < 5; i++)
+            {
+                y = i - 1;
+                x = 1 * 10;
+                Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
