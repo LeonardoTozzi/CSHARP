246e7e3b6155022962c895b227bed3b93f507088
LeonardoTozzi <leonardotozzis@gmail.com>
2025-02-17 13:23:39 -0300

    Foreach

---
+++ Pratica02/PraticaForeach.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace PraticaForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            N = int.Parse(Console.ReadLine());
+            string[] vet = new string[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                vet[x] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Nomes lidos: ");
+            foreach (string x in vet)
+            {
+                Console.WriteLine(vet[x]);
+            }
+            Console.ReadLine();
+
+              
+        }
+    }
+}
